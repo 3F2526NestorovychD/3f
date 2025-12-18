c0212647a31e683949eef8724d69d9d3435c8d7d
3FSultaniE <sultani.erfan.2023@itisgalileiroma.it>
2025-12-18 21:16:45 +0100

    vb

---
+++ 20-11-2025_compiti/esercizio_pag_68-69 copy.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const float CK = 1.2;
+void importo()  {
+    char nome[15];
+    float km_iniziali, km_finali, km_percorsi, importo;
+
+    printf("Inserisci il tuo nome:\n");
+    scanf("%15s", nome);
+
+    printf("Inserisci i chilometri iniziali dell'automobile:\n");
+    scanf("%f", &km_iniziali);
+
+    printf("Inserisci i chilometri finali dell'automobile:\n");
+    scanf("%f", &km_finali);
+
+    km_percorsi = km_finali - km_iniziali;
+    importo = km_percorsi * CK;
+
+    printf("Gentile %s, hai percorso %.2f km e devi pagare %.2f euro.\n", nome, km_percorsi, importo);
+    printf("Grazie per aver scelto il nostro servizio, siamo aperti 24 su 24 ore.\n");
+
+    
+}
+int main() {
+    importo();
+    system("pause");  
+    
+    return 0;
+    }
